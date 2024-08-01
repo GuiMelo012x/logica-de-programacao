ea5c5e60ef586ecd967ba647b1f4c4e157df4ebe
Guilherme Melo <guilherme2020102296@unicap.br>
2024-08-01 15:06:48 -0300

    commit 1

---
+++ C/Introdução/p2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Este eh um teste 2 de um programa em C !");
+    return 0;
+}
---
+++ C/Introdução/primeiroPrograma.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Este eh um teste de um programa em C !");
+    return 0;
+}
